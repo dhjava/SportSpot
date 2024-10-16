ea45cc1b06bcd382ecd387759de380887af55d8b
dhjava <spaghetti0119@gmail.com>
2024-10-16 18:30:50 +0900

    프로젝트 시작

---
+++ src/main/java/com/dh/sportspot/SportspotApplication.java
@@ -0,0 +1,13 @@
+package com.dh.sportspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/sportspot/SportspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.sportspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
