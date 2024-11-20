4a2a7edac0b4c9d90eb2f2be52e496fd923047c5
dhjava <spaghetti0119@gmail.com>
2024-11-20 23:59:32 +0900

    241120 view.html 제작

--- src/main/java/com/dh/sportspot/Controller/FacilityController.java
+++ src/main/java/com/dh/sportspot/Controller/FacilityController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/facility")
 public class FacilityController {
-    @GetMapping("/list")
+    @GetMapping("/futsal/list")
     public String list() {
-        return "facility/list";
+        return "facility/futsal/list";
+    }
+
+    @GetMapping("/futsal/view")
+    public String view() {
+        return "facility/futsal/view";
     }
 }
