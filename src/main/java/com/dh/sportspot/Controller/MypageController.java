2fcc0b0cf5b87f2781e36fc18011a2e77caa780a
dhjava <spaghetti0119@gmail.com>
2024-11-24 22:19:18 +0900

    241124 페이지 완성

---
+++ src/main/java/com/dh/sportspot/Controller/MypageController.java
@@ -0,0 +1,30 @@
+package com.dh.sportspot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class MypageController {
+
+    @GetMapping("/modify")
+    public String modify() {
+        return "mypage/modify";
+    }
+
+    @GetMapping("/memberLeave")
+    public String memeberLeave() {
+        return "mypage/memberLeave";
+    }
+
+    @GetMapping("/myContents/myPlace")
+    public String myPlace() {
+        return "mypage/myContents/myPlace";
+    }
+
+    @GetMapping("/myContents/bookmark")
+    public String bookmark() {
+        return "mypage/myContents/bookmark";
+    }
+}
